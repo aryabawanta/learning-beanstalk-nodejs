623e65f92f6a6e519b3537039db394aacc95aa11
aryabawanta <arya.bawanta@gmail.com>
2020-01-20 21:53:26 +0800

    test

--- jobs/addjoblaravel.js
+++ jobs/addjoblaravel.js
@@ -36,6 +36,8 @@ client.use('default').onSuccess(function (data) {
     command.__addAttr__("chainConnection", "string", null, "null");
     command.__addAttr__("chainQueue", "string", null, "null");
     command.__addAttr__("delay", "string", null, "null");
+    command.__addAttr__("middleware", "string", null, "object");
+    command.__addAttr__("chained", "string", null, "object");
 
     command = serialize(command, "object");
 
