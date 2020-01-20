afc3aa700162695050f6eed341b1105ed7310397
aryabawanta <arya.bawanta@gmail.com>
2020-01-20 19:00:55 +0800

    init

---
+++ jobs/addjoblaravel.js
@@ -0,0 +1,14 @@
+var bs = require('nodestalker'),
+    client = bs.Client('127.0.0.1:11300');
+
+client.use('default').onSuccess(function (data) {
+    console.log(data);
+    var payload = {
+        job: 'App\\Jobs\\MyTestJob@handle',
+        data: { user: 'Arya Bawanta', email: 'arya.bawanta@gmail.com' }
+    };
+    client.put(payload).onSuccess(function (data) {
+        console.log(data);
+        client.disconnect();
+    });
+});
