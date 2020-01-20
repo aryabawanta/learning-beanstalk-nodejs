17a32b853772201e362a1b3c1e688db110cb134e
aryabawanta <arya.bawanta@gmail.com>
2020-01-20 20:11:42 +0800

    testing

--- jobs/addjoblaravel.js
+++ jobs/addjoblaravel.js
@@ -7,7 +7,8 @@ client.use('default').onSuccess(function (data) {
         job: 'App\\Jobs\\Node@handle',
         data: "Ubuntu"
     });
-    client.put(payload).onSuccess(function (data) {
+    var laraveljob = '{"displayName":"App\\\\Jobs\\\\FindFavoriteOS","job":"Illuminate\\\\Queue\\\\CallQueuedHandler@call","maxTries":null,"delay":null,"timeout":null,"timeoutAt":null,"data":{"commandName":"App\\\\Jobs\\\\FindFavoriteOS","command":"O:23:\\"App\\\\Jobs\\\\FindFavoriteOS\\":8:{s:6:\\"\\u0000*\\u0000job\\";N;s:10:\\"connection\\";N;s:5:\\"queue\\";N;s:15:\\"chainConnection\\";N;s:10:\\"chainQueue\\";N;s:5:\\"delay\\";N;s:10:\\"middleware\\";a:0:{}s:7:\\"chained\\";a:0:{}}"}}';
+    client.put(laraveljob).onSuccess(function (data) {
         console.log(data);
         client.disconnect();
     });
